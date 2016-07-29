ea2ba07aeae685b6940665b70e56f112bd608f5b
Mihai Emil Andronache <amihaiemil@gmail.com>
2016-07-29 16:48:11 +0300

    Create githubContributors.js

---
+++ js/githubContributors.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    getContributors(
+	    [
+		    "https://api.github.com/repos/amihaiemil/charles-github-ejb/contributors",
+			"https://api.github.com/repos/amihaiemil/charles/contributors"
+		]
+	);
+});
+function getContributors(contributorsUrls, successCallback) {
+	$.support.cors = true;
+	for (var index in contributorsUrls) {
+        $.ajax({
+		    type : "GET",
+		    url : contributorsUrl,
+		    dataType : 'json',
+		    contentType : "application/json; charset=utf-8",
+		    headers : {
+			    Accept : "application/json; charset=utf-8"
+		    },
+		    success : function(contributors, status) {
+			    if(status == "success") {
+			        successCallback(contributors);
+			    } else {
+			        failedGithubCall();
+			    }
+		    },
+		    statusCode: {
+			    404: function() {
+                    failedGithubCall();
+			    },
+			    500: function() {
+                    failedGithubCall();
+			    }
+		    },
+		    error : function(e) {
+                failedGithubCall();
+		    }
+	    });
+	}
+}
+
+/**
+* Call this function in case the call to Github API did not succeed.
+**/
+function failedGithubCall() {
+    $("#nogithub").show();
+	$("#githubok").hide();
+}
+
+/**
+* Do something with the retreived contributors.
+**/
+function contributorsCallback(contributors) {
+	$("#nogithub").hide();
+    $("#githubok").show();
+	for(var index in contributors) {
+		var authorBubbleId = "#" + contributors[index].login;
+		if($(authorBubbleId).length == 0)) {//Check if the contributor wasn't already added on the page with a previous call.
+	        var authorBubble =
+		        "<li><a title='" + contributors[index].login + " Avatar'target='_blank' href='" + contributors[index].html_url + "'><img class='img-responsive' src='" + contributors[index].avatar_url + "'></a></li>";
+			$('#contributors').append(authorTableEntry);
+		}
+	}
+}
