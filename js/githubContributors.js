5201c71c96c31ad6690d748c4a7247d75ddc78c4
Mihai Emil Andronache <amihaiemil@gmail.com>
2016-07-29 16:52:12 +0300

    Update githubContributors.js

--- js/githubContributors.js
+++ js/githubContributors.js
@@ -55,7 +55,7 @@ function contributorsCallback(contributors) {
     $("#githubok").show();
 	for(var index in contributors) {
 		var authorBubbleId = "#" + contributors[index].login;
-		if($(authorBubbleId).length == 0)) {//Check if the contributor wasn't already added on the page with a previous call.
+		if($(authorBubbleId).length == 0) {//Check if the contributor wasn't already added on the page with a previous call.
 	        var authorBubble =
 		        "<li><a title='" + contributors[index].login + " Avatar'target='_blank' href='" + contributors[index].html_url + "'><img class='img-responsive' src='" + contributors[index].avatar_url + "'></a></li>";
 			$('#contributors').append(authorTableEntry);
