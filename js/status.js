6f39060523a39400febed4bacda118e5b9ef33c3
amihaiemil <amihaiemil@gmail.com>
2016-08-29 09:59:21 +0300

    title for status badge

--- js/status.js
+++ js/status.js
@@ -15,21 +15,21 @@ function getServiceStatus(statusUrl) {
 		success : function(response, status) {
 			var lastUpdated;
 			if(status == "success") {
-                $("#statusBadge").html('<img alt="Status badge" src="img/service status-online-green.svg"/>');
+                $("#statusBadge").html('<img alt="Status badge" title="The service is up and running" src="img/service status-online-green.svg"/>');
 			} else {
-			    $("#statusBadge").html('<img alt="Status badge" src="img/service status-offline-yellow.svg"/>');
+			    $("#statusBadge").html('<img alt="Status badge" title="The service is currently offline" src="img/service status-offline-yellow.svg"/>');
 			}
 		},
 		statusCode: {
 			404: function() {
-			    $("#statusBadge").html('<img alt="Status badge" src="img/service status-offline-yellow.svg"/>');
+			    $("#statusBadge").html('<img alt="Status badge" title="The service is currently offline" src="img/service status-offline-yellow.svg"/>');
 			},
 			500: function() {
-			    $("#statusBadge").html('<img alt="Status badge" src="img/service status-offline-yellow.svg"/>');
+			    $("#statusBadge").html('<img alt="Status badge" title="The service is currently offline" src="img/service status-offline-yellow.svg"/>');
 			}
 		},
 		error : function(e) {
-			$("#statusBadge").html('<img alt="Status badge" src="img/service status-offline-yellow.svg"/>');
+			$("#statusBadge").html('<img alt="Status badge" title="The service is currently offline" src="img/service status-offline-yellow.svg"/>');
 		}
 	});
 }
