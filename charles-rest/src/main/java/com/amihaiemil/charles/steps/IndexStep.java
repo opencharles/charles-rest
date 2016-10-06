2072e99a2fb79f58ae5b8ec0759a85bc653c805f
amihaiemil <amihaiemil@gmail.com>
2016-10-06 15:37:23 +0300

    javadocs

--- charles-rest/src/main/java/com/amihaiemil/charles/steps/IndexStep.java
+++ charles-rest/src/main/java/com/amihaiemil/charles/steps/IndexStep.java
@@ -1,3 +1,27 @@
+/*
+ * Copyright (c) 2016, Mihai Emil Andronache
+ * All rights reserved.
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  1)Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *  2)Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *  3)Neither the name of charles-rest nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.amihaiemil.charles.steps;
 
 import org.openqa.selenium.WebDriver;
@@ -5,12 +29,27 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriverService;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+/**
+ * Base class for index steps.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 1.0.0
+ *
+ */
 public abstract class IndexStep extends IntermediaryStep{
 
+	/**
+	 * Ctor.
+	 * @param next Next step to take.
+	 */
 	public IndexStep(Step next) {
 		super(next);
 	}
 
+	/**
+	 * Use phantomjs to fetch the web content.
+	 * @return
+	 */
 	protected WebDriver phantomJsDriver() {
 		String phantomJsExecPath =  System.getProperty("phantomjsExec");
 	    if(phantomJsExecPath == null || "".equals(phantomJsExecPath)) {
@@ -24,5 +63,5 @@ public abstract class IndexStep extends IntermediaryStep{
         );
         return new PhantomJSDriver(dc);
 	}
-	
+
 }
