a13edcfffee989ef2b1be03849ce2c264f297254
amihaiemil <amihaiemil@gmail.com>
2016-12-25 13:48:34 +0200

    updated aws-java-sdk and added checkstyle

--- src/main/java/com/amihaiemil/charles/aws/EsHttpRequest.java
+++ src/main/java/com/amihaiemil/charles/aws/EsHttpRequest.java
@@ -1,10 +1,10 @@
-/*
+/**
  * Copyright (c) 2016, Mihai Emil Andronache
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *  1)Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
+ *  1)Redistributions of source code must retain the above copyright notice,
+ *  this list of conditions and the following disclaimer.
  *  2)Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
@@ -13,18 +13,17 @@
  * this software without specific prior written permission.
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 package com.amihaiemil.charles.aws;
-
-import java.net.URI;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.ClientConfiguration;
@@ -34,6 +33,7 @@ import com.amazonaws.Response;
 import com.amazonaws.http.AmazonHttpClient;
 import com.amazonaws.http.ExecutionContext;
 import com.amazonaws.http.HttpResponseHandler;
+import java.net.URI;
 
 /**
  * Base HTTP request sent to Amazon ES service using the aws-java-sdk.
@@ -66,7 +66,7 @@ public final class EsHttpRequest<T> implements AwsHttpRequest<T> {
      * @param errHandle Error handler.
      */
     public EsHttpRequest(
-    	String uri,
+        String uri,
         HttpResponseHandler<T> respHandler,
         HttpResponseHandler<AmazonServiceException> errHandler
     ){
@@ -75,10 +75,10 @@ public final class EsHttpRequest<T> implements AwsHttpRequest<T> {
         if(esEndpoint == null || esEndpoint.isEmpty()) {
             throw new IllegalStateException("ElasticSearch endpoint needs to be specified!");
         }
-    	if(esEndpoint.endsWith("/")) {
-    		esEndpoint += uri;
+        if(esEndpoint.endsWith("/")) {
+            esEndpoint += uri;
         } else {
-        	esEndpoint += "/" + uri;
+            esEndpoint += "/" + uri;
         }
         this.request.setEndpoint(URI.create(esEndpoint));
         
@@ -91,11 +91,13 @@ public final class EsHttpRequest<T> implements AwsHttpRequest<T> {
      * Perform this request.
      */
     public T perform() {
-        Response<T> r = new AmazonHttpClient(new ClientConfiguration())
-                            .execute(
-                                this.request, new ExecutionContext(true), this.respHandler, this.errHandler
-                            );
-        return r.getAwsResponse();
+        final Response<T> rsp = new AmazonHttpClient(new ClientConfiguration())
+            .requestExecutionBuilder()
+            .executionContext(new ExecutionContext(true))
+            .request(this.request)
+            .errorResponseHandler(this.errHandler)
+            .execute(this.respHandler);
+        return rsp.getAwsResponse();
     }
 
     /**
