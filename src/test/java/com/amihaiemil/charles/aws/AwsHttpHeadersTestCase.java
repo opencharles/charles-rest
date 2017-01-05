e3be7e56a85b9d0367e9323d5c9c9a77f5337c83
amihaiemil <amihaiemil@gmail.com>
2017-01-05 22:03:59 +0200

    updated years

--- src/test/java/com/amihaiemil/charles/aws/AwsHttpHeadersTestCase.java
+++ src/test/java/com/amihaiemil/charles/aws/AwsHttpHeadersTestCase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, Mihai Emil Andronache
+ * Copyright (c) 2016-2017, Mihai Emil Andronache
  * All rights reserved.
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
