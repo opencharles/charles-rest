150ae4c17ee862d2d6b1a8b04bc993a94ba5b371
amihaiemil <amihaiemil@gmail.com>
2017-01-19 16:22:43 +0200

    test pdd

--- src/main/java/com/amihaiemil/charles/github/Command.java
+++ src/main/java/com/amihaiemil/charles/github/Command.java
@@ -41,7 +41,9 @@ import com.jcabi.http.response.JsonResponse;
  * @author Mihai Andronache (amihaiemil@gmail.com)
  * @version $Id$
  * @since 1.0.0
- * 
+ * @todo #176:1h/DEV Test puzzle to test if 0pdd works for
+ *  this repository? Last puzzle didn't generate any Github
+ *  issue.
  */
 public abstract class Command {
 
