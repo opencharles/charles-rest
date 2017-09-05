f009679a759dde0f5421cd8231d3b597f9b9be99
amihaiemil <amihaiemil@gmail.com>
2017-09-05 10:01:43 +0300

    formatting

--- src/main/java/com/amihaiemil/charles/rest/NotificationsResource.java
+++ src/main/java/com/amihaiemil/charles/rest/NotificationsResource.java
@@ -136,31 +136,31 @@ public class NotificationsResource {
     @Path("/github/issuecomment")
     @Consumes(MediaType.APPLICATION_JSON)
     public Response webhook(JsonObject issueComment) {
-    	final String event = this.request.getHeader("X-Github-Event");
-    	String userAgent = this.request.getHeader("User-Agent");
-    	if(userAgent == null) {
-    		userAgent = "";
-    	}
-    	if(userAgent.startsWith("GitHub-Hookshot/")) {
-			if("ping".equalsIgnoreCase(event)) {
-				return Response.ok().build();
-			}
-			if("issue_comment".equals(event)) {
-				Notification notification = new Notification();
-				notification.setIssueNumber(
-				    issueComment.getJsonObject("issue").getInt("number")
-				);
-				notification.setRepoFullName(
-			    	issueComment.getJsonObject("repository").getString("full_name")
-			    );
-				List<Notification> notifications = new ArrayList<>();
-				notifications.add(notification);
-			    boolean startedHandling = this.handleNotifications(notifications);
-			    if(startedHandling) {
-			        return Response.ok().build();
-			    }
-		    }
-    	}
+        final String event = this.request.getHeader("X-Github-Event");
+        String userAgent = this.request.getHeader("User-Agent");
+        if(userAgent == null) {
+            userAgent = "";
+        }
+        if(userAgent.startsWith("GitHub-Hookshot/")) {
+            if("ping".equalsIgnoreCase(event)) {
+                return Response.ok().build();
+            }
+            if("issue_comment".equals(event)) {
+                Notification notification = new Notification();
+                notification.setIssueNumber(
+                    issueComment.getJsonObject("issue").getInt("number")
+                );
+                notification.setRepoFullName(
+                    issueComment.getJsonObject("repository").getString("full_name")
+                );
+                List<Notification> notifications = new ArrayList<>();
+                notifications.add(notification);
+                boolean startedHandling = this.handleNotifications(notifications);
+                if(startedHandling) {
+                    return Response.ok().build();
+                }
+            }
+        }
         return Response.status(HttpURLConnection.HTTP_PRECON_FAILED).build();
     }
     
@@ -205,6 +205,6 @@ public class NotificationsResource {
      */
     @Asynchronous
     private void take(Action action) {
-    	action.perform();
+        action.perform();
     }
 }
